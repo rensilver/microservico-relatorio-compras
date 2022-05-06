70eedc48a9b07c38785afde6d9f6f5f9efe1e47d
rensilver <ren.oliv87@gmail.com>
2022-05-06 16:43:49 -0300

    ms-relatorio-compras projeto criado

---
+++ ms-relatorio-compras/src/main/java/com/rensilver/msrelatoriocompras/MsRelatorioComprasApplication.java
@@ -0,0 +1,13 @@
+package com.rensilver.msrelatoriocompras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsRelatorioComprasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsRelatorioComprasApplication.class, args);
+	}
+
+}
---
+++ ms-relatorio-compras/src/test/java/com/rensilver/msrelatoriocompras/MsRelatorioComprasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rensilver.msrelatoriocompras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRelatorioComprasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
